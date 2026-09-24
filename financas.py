938db30c5acbfc663bce917e18bcb96328f53f80
Nelson Duarte <for194245@formacao.iefp.pt>
2026-09-24 11:58:50 +0100

    criação de um dataframe para apresentar a media dos salarios por idade com a biblioteca pandas e numpy

--- financas.py
+++ financas.py
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 import pandas as pd
 import yfinance as yf
+import numpy as np
 
 # Baixar dados históricos de ações da Apple usando yfinance
 df = yf.download('AAPL', start='2026-01-01', end='2026-08-31')
@@ -11,3 +14,13 @@ print()
 # Calcular a média móvel simples de 20 dias
 df['SMA_20'] = df['Close'].rolling(window=20).mean()
 print(df[['Close', 'SMA_20']].tail())
+
+# Criar um programa que carrega um dataframe que tenha os nomes, idades e salários de funcionários, e calcula o salário médio por idade.
+df_funcionarios = pd.DataFrame({
+    'nome': ['Alice', 'Manel', 'Bob', 'Eva', 'Francis', 'Charlie', 'David', 'Grace', 'Hannah', 'Ian'],
+    'idade': [25, 25, 30, 30, 30, 35, 35, 40, 40, 40],
+    'salário': [50000, 35000, 65000, 58000, 60000, 70000, 75000, 80000, 83000, 90000]
+})
+
+salario_medio_por_idade = df_funcionarios.groupby('idade')['salário'].mean()
+print(salario_medio_por_idade)
