f691185d4d4539ef935cd86151656a1180b23661
Nelson Duarte <for194245@formacao.iefp.pt>
2026-09-24 12:46:49 +0100

    Inserção de codigo para imprimir uma string com o  salario medio

--- financas.py
+++ financas.py
@@ -25,3 +25,7 @@ df_funcionarios = pd.DataFrame({
 
 salario_medio_por_idade = df_funcionarios.groupby('idade')['salário'].mean()
 print(salario_medio_por_idade)
+
+print("O salário médio por idade é:")
+for idade, salario_medio in salario_medio_por_idade.items():
+    print(f"Idade: {idade}, Salário Médio: {salario_medio:.2f}")
